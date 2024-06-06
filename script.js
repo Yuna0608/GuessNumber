a64f355bd75532825d6f0e96626c8f42e5bb160a
Yuna0608 <90213895+Yuna0608@users.noreply.github.com>
2024-06-06 10:59:22 +0800

    Add files via upload
    陽春版

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var random = GetRandomNum(1, 100);
 
-window.onload = SetAnswerLabel;
+window.onload = function () {
+    SetAnswerLabel();
+    inputEvent();
+}
 
 function GetRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function SetAnswerLabel() {
-    console.error("123");
     var ansLabel = document.getElementById("answer");
     if (ansLabel)
         ansLabel.textContent = `Ans: ${random}`;
@@ -16,12 +18,34 @@ function SetAnswerLabel() {
 }
 
 function CheckNum() {
-    // var input = document.getElementById("inputNum").value;
-    // var result = document.getElementById("result");
-    // if (random > input)
-    //     result.textContent = "太小了";
-    // else if (random < input)
-    //     result.textContent = "太大了";
-    // else
-    //     result.textContent = "答對了!!";
+    var result = document.getElementById("result");
+    var btnOK = document.getElementById("ok");
+    var input = document.getElementById("inputNum").value;
+    if (btnOK.textContent == "再來一局") {
+        result.textContent = "";
+        btnOK.textContent = "送出";
+        random = GetRandomNum(1, 100);
+        SetAnswerLabel();
+    }
+    else {
+        if (random > input)
+            result.textContent = "太小了";
+        else if (random < input)
+            result.textContent = "太大了";
+        else if (random == input) {
+            result.textContent = "答對了!!";
+            btnOK.textContent = "再來一局"
+        }
+        else
+            result.textContent = "請輸入1~100之間的整數";
+    }
+}
+
+function inputEvent() {
+    document.getElementById("inputNum").addEventListener("keydown", function (event) {
+        if (event.key == "Enter") {
+            event.preventDefault();
+            document.getElementById("ok").click();
+        }
+    });
 }
