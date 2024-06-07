7a771a555aaeaad89cf4485242064e064b4f0f1c
Yuna0608 <90213895+Yuna0608@users.noreply.github.com>
2024-06-07 09:50:19 +0800

    Add files via upload
    add history message

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var random = GetRandomNum(1, 100);
 
 window.onload = function () {
     SetAnswerLabel();
-    inputEvent();
+    InputEvent();
 }
 
 function GetRandomNum(min, max) {
@@ -20,32 +20,53 @@ function SetAnswerLabel() {
 function CheckNum() {
     var result = document.getElementById("result");
     var btnOK = document.getElementById("ok");
-    var input = document.getElementById("inputNum").value;
+    var inputNum = document.getElementById("inputNum");
+    var input = inputNum.value;
+    input = parseInt(input);
+    inputNum.value = '';
+
     if (btnOK.textContent == "再來一局") {
-        result.textContent = "";
         btnOK.textContent = "送出";
+        result.textContent = "";
         random = GetRandomNum(1, 100);
         SetAnswerLabel();
+        ClearHistory();
     }
     else {
-        if (random > input)
+        if (isNaN(input) || input < 1 || input > 100) {
+            result.textContent = "請輸入1~100之間的整數";
+            return;
+        }
+        else if (random > input)
             result.textContent = "太小了";
         else if (random < input)
             result.textContent = "太大了";
-        else if (random == input) {
+        else {
             result.textContent = "答對了!!";
             btnOK.textContent = "再來一局"
         }
-        else
-            result.textContent = "請輸入1~100之間的整數";
+        var message = `${input}: ${result.textContent}`;
+        AddHistory(message);
     }
 }
 
-function inputEvent() {
+function InputEvent() {
     document.getElementById("inputNum").addEventListener("keydown", function (event) {
         if (event.key == "Enter") {
             event.preventDefault();
             document.getElementById("ok").click();
         }
     });
+}
+
+function AddHistory(message) {
+    var historyContainer = document.getElementById("historyContainer");
+    var newHistory = document.createElement('p');
+    newHistory.textContent = message;
+    historyContainer.appendChild(newHistory);
+}
+
+function ClearHistory() {
+    var historyContainer = document.getElementById("historyContainer");
+    historyContainer.textContent = '';
 }
